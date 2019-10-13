6a2e460758fba2c6e6f122a1eaf5c51af6c9380c
AhmedTorres-Mobile <ahmedtorres366@gmail.com>
2019-10-13 04:44:28 -0700

    a

--- lib/review/allReviews.dart
+++ lib/review/allReviews.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:big/componets/appBar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:big/model/Review.dart';
 
-class AllReviews extends StatelessWidget {
+class AllReviews extends StatefulWidget {
+  @override
+  State<AllReviews> createState() {
+    return AllReviewsState();
+  }
+}
+
+class AllReviewsState extends State<AllReviews> {
+
+
+  Future<List<Review>> getReviews() async {
+    final res = await http.get("http://18.217.190.199/api/product/20/reviews");
+    List<Review> list = <Review>[];
+
+    if (res.statusCode == 200) {
+      var data = json.decode(res.body);
+
+      var restdata = data["data"][0];
+
+      print("review data"+restdata.toString());
+
+      list = restdata.map<Review>((json) => Review.fromJson(json)).toList();
+            print("review dataaaaaaaaaaaaaaaaaaaaaaaaaaa"+list.toString());
+
+    }
+    return list;
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    getReviews();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,49 +67,54 @@ class AllReviews extends StatelessWidget {
               Container(
                 padding: EdgeInsets.all(8),
                 height: MediaQuery.of(context).size.height * .7,
-                child: ListView.builder(
-                  itemCount: 12,
-                  itemBuilder: (context, position) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Text(
-                                    'Ahmed Torres',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    '1h ago',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  )
-                                ],
-                              ),
-                              SmoothStarRating(
-                                  allowHalfRating: false,
-                                  starCount: 5,
-                                  rating: position.toDouble(),
-                                  size: 20.0,
-                                  color: Colors.yellow,
-                                  borderColor: Colors.grey,
-                                  spacing: 0.0),
-                              Text(
-                                'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor…....',
-                                style: TextStyle(fontSize: 14.0),
-                              )
-                            ]),
-                      ),
-                    );
-                  },
-                ),
+                child: FutureBuilder<List<Review>>(
+                    future: getReviews(),
+                    builder: (context, snapshot) {
+                      List<Review> reviewList = snapshot.data;
+                      return ListView.builder(
+                        itemCount: reviewList.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text(
+                                          reviewList[index].customer.name,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          '1h ago',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    ),
+                                    SmoothStarRating(
+                                        allowHalfRating: false,
+                                        starCount: 5,
+                                        rating: reviewList[index].rate.toDouble(),
+                                        size: 20.0,
+                                        color: Colors.yellow,
+                                        borderColor: Colors.grey,
+                                        spacing: 0.0),
+                                    Text(reviewList[index].content,
+                                      style: TextStyle(fontSize: 14.0),
+                                    )
+                                  ]),
+                            ),
+                          );
+                        },
+                      );
+                    }),
               ),
             ],
           ),
@@ -81,7 +124,46 @@ class AllReviews extends StatelessWidget {
   }
 }
 
+
+Future<dynamic> setReview() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var userToken=await prefs.getString('userToken');
+  print(userToken);
+    final res = await http.post("http://18.217.190.199/api/product/20/reviews",
+    body: {
+      
+	"customer_id":"$userToken",
+	"rate":_rate.toString(),
+	"content":contentController.text.toString()
+
+    });
+
+    if (res.statusCode == 200) {
+      var data = json.decode(res.body);
+
+      var restdata = data["data"];
+
+      print("review data"+restdata.toString());
+
+
+    }
+  }
+
+double _rate=0.0;
+  final contentController = TextEditingController();
+
+
+
+
 openAlertBox(BuildContext context) {
+
+
+
+onchangerate(double r){
+_rate=r;
+print(r.toString());
+}
+
   return showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -105,16 +187,15 @@ openAlertBox(BuildContext context) {
                     ),
                   ),
                   Center(
-                    child:
-                      SmoothStarRating(
-                          allowHalfRating: false,
-                          starCount: 5,
-                          rating: 3,
-                          size: 20.0,
-                          color: Colors.yellow,
-                          borderColor: Colors.grey,
-                          spacing: 0.0),
-                    
+                    child: SmoothStarRating(
+                        allowHalfRating: false,
+                        starCount: 5,
+                        rating: _rate,
+                        onRatingChanged:onchangerate,
+                        size: 20.0,
+                        color: Colors.yellow,
+                        borderColor: Colors.grey,
+                        spacing: 0.0),
                   ),
                   SizedBox(
                     height: 5.0,
@@ -126,15 +207,18 @@ openAlertBox(BuildContext context) {
                   Padding(
                     padding: EdgeInsets.only(left: 30.0, right: 30.0),
                     child: TextField(
+                      controller: contentController,
                       decoration: InputDecoration(
                         hintText: "Write your review",
                         border: InputBorder.none,
                       ),
-                      maxLines: 
-                      6,
+                      maxLines: 6,
                     ),
                   ),
                   InkWell(
+                    onTap: (){print("AAAAAAAAAAA");
+                    setReview();
+                    },
                     child: Container(
                       padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
                       decoration: BoxDecoration(
